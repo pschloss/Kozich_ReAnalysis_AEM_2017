5a4196bc89c1e087a2f9d81fe137efb286db0d96
Pat Schloss <pschloss@umich.edu>
2017-03-01 09:28:46 -0500

    Process data through mothur MiSeq SOP

--- analysis_driver.bash
+++ analysis_driver.bash
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 # Download the raw data and put them into the data/raw directory
-curl -LO https://www.mothur.org/MiSeqDevelopmentData/StabilityWMetaG.tar
-tar xvf StabilityWMetaG.tar -C data/raw/
-rm StabilityWMetaG.tar
+curl -LO https://www.mothur.org/MiSeqDevelopmentData/StabilityNoMetaG.tar
+tar xvf StabilityNoMetaG.tar -C data/raw/
+rm StabilityNoMetaG.tar
 
 
 # Download the SILVA reference file (v.123). We will pull out the bacteria-specific sequences and
 # clean up the directories to remove the extra files
 curl -LO http://mothur.org/w/images/1/15/Silva.seed_v123.tgz
 tar xvzf Silva.seed_v123.tgz silva.seed_v123.align silva.seed_v123.tax
-mothur "#get.lineage(fasta=silva.seed_v123.align, taxonomy=silva.seed_v123.tax, taxon=Bacteria);degap.seqs(fasta=silva.seed_v123.pick.align, processors=8)"
+code/mothur/mothur "#get.lineage(fasta=silva.seed_v123.align, taxonomy=silva.seed_v123.tax, taxon=Bacteria);degap.seqs(fasta=silva.seed_v123.pick.align, processors=8)"
 mv silva.seed_v123.pick.align data/references/silva.seed.align
 rm Silva.seed_v123.tgz silva.seed_v123.*
 rm mothur.*.logfile
@@ -31,3 +31,20 @@ unzip Mothur.mac_64.OSX-10.12.zip
 mv mothur code/
 rm Mothur.mac_64.OSX-10.12.zip
 rm -rf __MACOSX
+
+
+# Generate a customized version of the SILVA reference database that targets the V4 region
+code/mothur/mothur "#pcr.seqs(fasta=data/references/silva.seed.align, start=11894, end=25319, keepdots=F, processors=8)"
+mv data/references/silva.seed.pcr.align data/references/silva.v4.align
+
+
+# Process raw sequences to the point where they are fully curated
+code/mothur/mothur code/get_good_seqs.batch
+
+
+# Calculate the sequencing error rate
+code/mothur/mothur code/get_error.batch
+
+
+# Generate the shared files
+code/mothur/mothur code/get_shared_otus.batch
